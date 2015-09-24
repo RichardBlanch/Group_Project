3cde8da06d9bbb2a1c945c65918fee73a2cd93c5
Rich Blanchard <blanchard5595@yahoo.com>
2015-09-23 20:27:25 -0600

    Initial Commit

---
+++ Study_FinderTests/Study_FinderTests.m
@@ -0,0 +1,39 @@
+//
+//  Study_FinderTests.m
+//  Study_FinderTests
+//
+//  Created by Rich Blanchard on 9/23/15.
+//  Copyright © 2015 Rich. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Study_FinderTests : XCTestCase
+
+@end
+
+@implementation Study_FinderTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
---
+++ Study_FinderUITests/Study_FinderUITests.m
@@ -0,0 +1,40 @@
+//
+//  Study_FinderUITests.m
+//  Study_FinderUITests
+//
+//  Created by Rich Blanchard on 9/23/15.
+//  Copyright © 2015 Rich. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Study_FinderUITests : XCTestCase
+
+@end
+
+@implementation Study_FinderUITests
+
+- (void)setUp {
+    [super setUp];
+    
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    // In UI tests it is usually best to stop immediately when a failure occurs.
+    self.continueAfterFailure = NO;
+    // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+    [[[XCUIApplication alloc] init] launch];
+    
+    // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // Use recording to get started writing UI tests.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+@end
