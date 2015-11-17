932b5761aba7422f84cc2756166ecc680e46339f
nshaf <nish1367@colorado.edu>
2015-11-16 20:45:18 -0700

    updated sql to json

--- WebMenu/mySQLtoJSON.php
+++ WebMenu/mySQLtoJSON.php
@@ -1,52 +1,41 @@
-if (!function_exists('json_encode'))
-{
-  function json_encode($a=false)
-  {
-    if (is_null($a)) return 'null';
-    if ($a === false) return 'false';
-    if ($a === true) return 'true';
-    if (is_scalar($a))
-    {
-      if (is_float($a))
-      {
-        // Always use "." for floats.
-        return floatval(str_replace(",", ".", strval($a)));
-      }
-
-      if (is_string($a))
-      {
-        static $jsonReplaces = array(array("\\", "/", "\n", "\t", "\r", "\b", "\f", '"'), array('\\\\', '\\/', '\\n', '\\t', '\\r', '\\b', '\\f', '\"'));
-        return '"' . str_replace($jsonReplaces[0], $jsonReplaces[1], $a) . '"';
-      }
-      else
-        return $a;
+<?php
+    //Create Database connection
+    $db = mysql_connect("localhost","root","password");
+    if (!$db) {
+        die('Could not connect to db: ' . mysql_error());
     }
-    $isList = true;
-    for ($i = 0, reset($a); $i < count($a); $i++, next($a))
-    {
-      if (key($a) !== $i)
-      {
-        $isList = false;
-        break;
-      }
+ 
+    //Select the Database
+    mysql_select_db("classDatabase",$db);
+    
+    //Replace * in the query with the column names.
+    $result = mysql_query("select * from csciClasses", $db);  
+    
+    //Create an array
+    $json_response = array();
+    
+    while ($row = mysql_fetch_array($result, MYSQL_ASSOC)) {
+        $row_array['DEPT'] = $row['DEPT'];
+        $row_array['CourseSubj'] = $row['CourseSubj'];
+        $row_array['SectNum'] = $row['SectNum'];
+        $row_array['SessnCode'] = $row['SessnCode'];
+        $row_array['ClassNum'] = $row['ClassNum'];
+        $row_array['Credits'] = $row['Credits'];
+        $row_array['CourseTitle'] = $row['CourseTitle'];
+        $row_array['ClassComp'] = $row['ClassComp'];
+        $row_array['Times'] = $row['Times'];
+        $row_array['Days'] = $row['Days'];
+        $row_array['RoomNum'] = $row['RoomNum'];
+        $row_array['InstructorLast'] = $row['InstructorLast'];
+        $row_array['InstructorFirst'] = $row['InstructorFirst'];
+        $row_array['MaxEnrl'] = $row['MaxEnrl'];
+       
+        //push the values in the array
+        array_push($json_response,$row_array);
     }
-    $result = array();
-    if ($isList)
-    {
-      foreach ($a as $v) $result[] = json_encode($v);
-      return '[' . join(',', $result) . ']';
-    }
-    else
-    {
-      foreach ($a as $k => $v) $result[] = json_encode($k).':'.json_encode($v);
-      return '{' . join(',', $result) . '}';
-    }
-  }
-}
-
-$query = mysql_query("SELECT * FROM csciCourses");
-$rows = array();
-while($row = mysql_fetch_assoc($query)) {
-    $rows[] = $row;
-}
-print json_encode($rows);
+    echo json_encode($json_response);
+    
+    //Close the database connection
+    fclose($db);
+ 
+?>
